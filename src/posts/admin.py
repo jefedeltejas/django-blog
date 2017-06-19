aa31c46d44107b46b2ec8d07441c4027efe9bd2f
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-19 16:05:15 -0500

    added filtering to admin panel

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ["title", "timestamp"]
+    list_display = ["title", "updated", "timestamp"]
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp"]
     class Meta:
         model = Post
 
