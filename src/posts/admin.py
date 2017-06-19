f580e7e6c79051b81ab3a9660615adb3549ee25d
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-19 16:29:34 -0500

    enhancing views.py

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -6,7 +6,9 @@ from .models import Post
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ["title", "updated", "timestamp"]
     list_display_links = ["updated"]
+    list_editable = ["title"]
     list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
     class Meta:
         model = Post
 
