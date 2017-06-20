3459b801479aa06dacc52b870a127434028d8c8e
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-20 09:07:16 -0500

    enhancing our list with a loop of objects

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+# Create them views here.
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1>Wuppud make a new post!!!!</h1>")
@@ -13,7 +14,9 @@ def post_detail(request):
     return render(request, "index.html", context)
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list": queryset,
         "title": "Yo mama"
     }
     return render(request, "index.html", context)
