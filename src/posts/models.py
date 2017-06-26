399a32a017927135fcf5d42f094ff5923689ae6b
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-26 09:53:10 -0500

    adding share links to post details

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
 
+from django.utils.text import slugify
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
@@ -13,6 +14,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
             null=True, blank=True,
             height_field="height_field",
@@ -36,3 +38,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    exists = Post.objects.filter(slug=slug).exists()
+    if exists:
+        slug = "%s-%s" %(slug, instance.id)
+    instance.slug = slug
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
