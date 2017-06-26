35e39e15c388d5efdc0fbe57954bbdb43c2a39ff
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-26 15:08:16 -0500

    added basic user permissions

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,13 +1,15 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -52,6 +54,8 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, instance=instance)
     if form.is_valid():
@@ -69,6 +73,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 def post_delete(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     instance.deleted()
     messages.success(request, "Dude, All your data was deleted!")
