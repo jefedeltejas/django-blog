fc0bfc6cc1a62fe3d3bb389715efa680a7bf234b
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-27 11:09:18 -0500

    fixed error in post detail

--- src/posts/models.py
+++ src/posts/models.py
@@ -36,6 +36,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    objects = PostManager()
+
     def __unicode__(self):
         return self.title
 
